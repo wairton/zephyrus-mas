b748e94ae11332b51e157d15c8834fd902da48eb
Wairton de Abreu <vvairton@gmail.com>
2012-09-10 20:24:49 -0300

    Added GUI main files.

---
+++ src/zephyrusAuxiliary.py
@@ -0,0 +1,9 @@
+#-*-coding:utf-8-*-
+#This work is under LGPL license, see the LICENSE.LGPL file for further details.
+
+import json
+import os
+from gui.auxiliarGUI import ZephyrusAuxiliary
+
+if __name__ == '__main__':
+	ZephyrusAuxiliary()
---
+++ src/zephyrusMain.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+#This work is under LGPL license, see the LICENSE.LGPL file for further details.
+
+from gui.principalGUI import Zephyrus
+
+if __name__ == '__main__':
+	Zephyrus()
