c89764abd28b209df4c8534fdfd473b16b1fc776
Wairton de Abreu Rebouças <wairton@yahoo.com.br>
2015-07-17 10:09:51 -0300

    Using a json file to represent addresses in the system

--- src/core/enderecos.py
+++ src/core/enderecos.py
@@ -1,45 +1,46 @@
 #-*-coding:utf-8-*-
 import sys
+import json
+
 
 class Enderecos(object):
-    def __init__(self, arqEnderecos):
-        linhas = open(arqEnderecos).readlines()
-        self.enderecos = {}
-        for l in linhas:
-            lsplit = l.split()
-            if len(lsplit) == 3: #FIXME: isso foi posto aqui para tentar mitigar (na verdade, esconder) um bug
-                participante, nome, endereco = l.split()
-                self.enderecos[participante] = nome, endereco
+    def __init__(self, config_file):
+        self.addresses = json.load(open(config_file))
+        # TODO validate file
 
     def participantes(self):
         """Uma lista de ..."""
-        return self.enderecos.keys()
+        return self.addresses.keys()
 
     def participante(self, participante):
         """Retorna a tupla (nome, endereco) de participante"""
-        found = self.enderecos.get(participante)
+        found = self.addresses.get(participante)
         return found[0] if not found == None else None
 
     def nomes(self):
         """Retorna os nomes de todos os participantes."""
-        return [(a, self.enderecos[a][0]) for a in self.enderecos.keys()]
+        return [(a, self.addresses[a][0]) for a in self.addresses.keys()]
 
     def nome(self, nomeBuscado):
-        for participante, (nome, endereco) in self.enderecos.iteritems():
+        for participante, (nome, endereco) in self.addresses.iteritems():
             if nome == nomeBuscado:
                 return participante, endereco
         return None
 
     def endereco(self, participante):
         try:
-            endereco = self.enderecos.get(participante)[1]
+            endereco = self.addresses.get(participante)[1]
         except:
             return None
         return endereco
 
     def get(self, participante):
-        return self.enderecos.get(participante)
+        return self.addresses.get(participante)
 
     def getNome(self, nome):
-        found = filter(lambda k:k[0] == nome, self.enderecos.values())
+        found = filter(lambda k:k[0] == nome, self.addresses.values())
         return found[0] if len(found) != 0 else None
+
+
+if __name__ == '__main__':
+    Enderecos(sys.argv[1])
