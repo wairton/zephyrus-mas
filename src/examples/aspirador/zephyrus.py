b05288ac3a00447b487a5d348ab11838e72f8aa7
Wairton de Abreu Rebouças <wairton@yahoo.com.br>
2016-09-07 10:22:31 -0300

    Retab file

--- src/examples/aspirador/zephyrus.py
+++ src/examples/aspirador/zephyrus.py
@@ -1,14 +1,14 @@
 #-*-coding:utf-8-*-
-import json
 import os
-from testador import TestadorAspirador
-from roteiro import Roteiro
 import sys
+import json
+
+from roteiro import Roteiro
+from testador import TestadorAspirador
 
 
 if __name__ == '__main__':
-	roteiro = Roteiro('batata')
-	roteiro.load(sys.argv[1])
-	testador = TestadorAspirador()
-	testador.inicializarSimulacao(roteiro)
-	
+    roteiro = Roteiro('batata')
+    roteiro.load(sys.argv[1])
+    testador = TestadorAspirador()
+    testador.inicializarSimulacao(roteiro)
