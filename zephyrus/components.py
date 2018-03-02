97d0e0c7c98e876fe5b9011643c976025d91b5f2
Wairton de Abreu <wairtonjr@gmail.com>
2018-03-02 17:51:52 -0300

    flake8 components module

--- zephyrus/components.py
+++ zephyrus/components.py
@@ -2,72 +2,9 @@ import functools
 import json
 import re
 
-from sys import exit
-
 
 class ComponentException(Exception):
     pass
-
-
-class Composable:
-    def as_component_set(self):
-        raise NotImplementedError
-
-
-class Component(Composable):
-    __slots__ = ['name', 'value']
-
-    def __init__(self, name='', value=0):
-        self.name = name
-        self.value = value
-
-    def __str__(self):
-        return self.name
-
-    def __repr__(self):
-        return "<Component {}: {}>".format(self.name, self.value)
-
-    def __eq__(self, other):
-        return self.value == other.value
-
-    def __lt__(self, other):
-        return self.value < other.value
-
-    def __get__(self):
-        return self.value
-
-    def __add__(self, other) -> ComponentSet:
-        return self.as_component_set() + other
-
-    def as_component_set(self):
-        return ComponentSet(self.value)
-
-
-class ComponentEnum:
-    _identifier_pattern = re.compile(r"^[A-Z][A-Z0-9_]*$")
-
-    @classmethod
-    def is_valid_name(cls, name):
-        return cls._identifier_pattern.match(name) is not None
-
-    def __init__(self, items):
-        value = 1
-        self.name_value = {}
-        self.value_name = {}
-        for item in items:
-            if not ComponentEnum.is_valid_name(item.upper()):
-                raise ComponentException("Invalid component name: {}".format(item))
-            component = Component(item.upper(), value)
-            setattr(self, component.name, component)
-            self.name_value[component.name] = component.value
-            self.value_name[component.value] = component.name
-            value <<= 1
-
-    def get_value_for(self, name):
-        return self.name_value[name]
-
-    def get_name_for(self, value):
-        return self.value_name[value]
 
 
 def cast_component_set(method):
@@ -76,6 +13,11 @@ def cast_component_set(method):
         other = other.as_component_set()
         return method(self, other)
     return wrapped
+
+
+class Composable:
+    def as_component_set(self):
+        raise NotImplementedError
 
 
 class ComponentSet(Composable):
@@ -144,16 +86,72 @@ class ComponentSet(Composable):
         return self
 
 
+class Component(Composable):
+    __slots__ = ['name', 'value']
+
+    def __init__(self, name='', value=0):
+        self.name = name
+        self.value = value
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return "<Component {}: {}>".format(self.name, self.value)
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def __get__(self):
+        return self.value
+
+    def __add__(self, other) -> ComponentSet:
+        return self.as_component_set() + other
+
+    def as_component_set(self):
+        return ComponentSet(self.value)
+
+
+class ComponentEnum:
+    _identifier_pattern = re.compile(r"^[A-Z][A-Z0-9_]*$")
+
+    @classmethod
+    def is_valid_name(cls, name):
+        return cls._identifier_pattern.match(name) is not None
+
+    def __init__(self, items):
+        value = 1
+        self.name_value = {}
+        self.value_name = {}
+        for item in items:
+            if not ComponentEnum.is_valid_name(item.upper()):
+                raise ComponentException("Invalid component name: {}".format(item))
+            component = Component(item.upper(), value)
+            setattr(self, component.name, component)
+            self.name_value[component.name] = component.value
+            self.value_name[component.value] = component.name
+            value <<= 1
+
+    def get_value_for(self, name):
+        return self.name_value[name]
+
+    def get_name_for(self, value):
+        return self.value_name[value]
+
+
 class ComponentManager:
-    def __init__(self, component_enum=enum):
-        self.enum = enum
+    def __init__(self, component_enum):
+        self.enum = component_enum
 
     @classmethod
     def from_filename(cls, filename):
         try:
             json_items = json.load(open(filename))
-        except:
-            raise ComponentException("Unable to load {}".format(config_filename))
+        except (IOError, json.JSONDecodeError):
+            raise ComponentException("Unable to load {}".format(filename))
         return cls.from_component_names(json_items)
 
     @classmethod
@@ -171,4 +169,3 @@ class ComponentManager:
     @classmethod
     def group_components(self, *components):
         return ComponentSet.from_component_list(components)
-
