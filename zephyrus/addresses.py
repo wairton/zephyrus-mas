887ec65c9c45b7f91812f49763a7586bae01cdae
Wairton de Abreu <wairtonjr@gmail.com>
2018-05-01 09:04:00 -0300

    Add aliases as property

--- zephyrus/addresses.py
+++ zephyrus/addresses.py
@@ -7,19 +7,23 @@ from zephyrus.exceptions import ImproperlyConfigured
 class Participants:
     def __init__(self, filename):
         try:
-            self.addresses = json.load(open(filename))
+            self._addresses = json.load(open(filename))
         except (FileNotFoundError, json.decoder.JSONDecodeError) as e:
             raise ImproperlyConfigured(e)
-        self.aliases = {v: k for k, v in self.addresses.items()}
+        self._aliases = {v: k for k, v in self._addresses.items()}
 
     def alias(self, address):
         return self.aliases[address]
 
+    @property
+    def aliases(self):
+        return list(self._addresses.keys())
+
     def address(self, alias):
-        return self.addresses[alias]
+        return self._addresses[alias]
 
     def __str__(self):
-        return '<Participants>: {}'.format(str(self.addresses))
+        return '<Participants>: {}'.format(str(self._addresses))
 
 
 if __name__ == '__main__':
