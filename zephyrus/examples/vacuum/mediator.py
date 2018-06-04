945498dbc3ca3d32371221f2ee5e44a06fa4e294
Wairton de Abreu <wairtonjr@gmail.com>
2018-06-04 09:56:08 -0300

    Add mediator for vacuum example.

---
+++ zephyrus/examples/vacuum/mediator.py
@@ -0,0 +1,16 @@
+from zephyrus.mediator import Mediator
+
+
+class VacuumMediator(Mediator):
+    def configure(self, content):
+        self.participants = content
+        self.connect_to_participants()
+
+
+if __name__ == '__main__':
+    import sys
+    import os
+    basedir = os.path.dirname(__file__)
+    args = [s if s.startswith("/") else os.path.join(basedir, s) for s in sys.argv[1:]]
+
+    VacuumMediator(*args).start()
