3940608107482fc21be229c43a97b0e0de352c31
Wairton de Abreu <wairtonjr@gmail.com>
2018-06-07 09:00:44 -0300

    filter for python before killing processes

--- zephyrus/utils.py
+++ zephyrus/utils.py
@@ -4,7 +4,7 @@ from subprocess import Popen, PIPE
 
 
 def killpython():
-    os.system("ps aux | grep {} > crap".format(sys.argv[1]))
+    os.system("ps aux | grep python | grep {} > crap".format(sys.argv[1]))
     for line in open('crap').readlines():
         os.system('kill {}'.format(line.strip().split()[1]))
     os.system("rm crap")
