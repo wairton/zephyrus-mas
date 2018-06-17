c4919afebce13f24edec25b3d900295e5f7a2eec
Wairton de Abreu <wairtonjr@gmail.com>
2018-06-17 10:52:31 -0300

    Fix environment loop for ZDT example.

--- zephyrus/examples/zdt/environment.py
+++ zephyrus/examples/zdt/environment.py
@@ -6,19 +6,29 @@ from zephyrus.message import Message
 
 class ZDTEnvironment(Environment):
     def mainloop(self):
-        msg = Message(
-            'environment', receiver='agent', message_type='PERCEIVED', content=self.places)
-        self.socket_send.send_string(str(msg))
-        logging.debug('Environment: received {}'.format(self.socket_receive.recv_string()))
-        logging.debug('Environment: agent, please stop')
-        self.socket_send.send_string(
-            str(Message('environment', message_type='STOP', receiver='agent')))
+        start_message = self.messenger.build_start_message(receiver='mediator')
+        self.socket_send.send_string(str(start_message))
+
+        while True:
+            msg = Message.from_string(self.socket_receive.recv_string())
+            logging.debug('Environment: received {}'.format(msg))
+            if msg.type == 'PERCEIVE':
+                reply = self.messenger.build_confirm_message(receiver=msg.sender, content=self.points)
+                self.socket_send.send_string(str(reply))
+            elif msg.type == 'RESULT':
+                logging.debug("Environment: received result from {}".format(msg.sender))
+                break
+            elif msg.type == 'CONFIG':
+                # this handles the case where start arrives before config message
+                self.configure(msg.content)
+            else:
+                logging.error("Environmnent: received an invalid message '{}'".format(msg))
         logging.debug("Environment: monitor, I'm stopping now")
-        self.socket_send.send_string(
-            str(Message('environment', message_type='STOP', receiver='mediator')))
+        stop_message = self.messenger.build_stop_message(receiver='mediator')
+        self.socket_send.send_string(str(stop_message))
 
     def configure(self, content):
-        self.places = content
+        self.points = content
 
 if __name__ == '__main__':
     import os
