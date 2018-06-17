870e853e1d00d5d2e027c81e52922ffdc029a49b
Wairton de Abreu <wairtonjr@gmail.com>
2018-06-17 11:58:00 -0300

    give a better name to the temporary file...

--- zephyrus/utils.py
+++ zephyrus/utils.py
@@ -4,8 +4,8 @@ from subprocess import Popen, PIPE
 
 
 def killpython():
-    os.system("ps aux | grep python | grep {} > crap".format(sys.argv[1]))
-    for line in open('crap').readlines():
+    os.system("ps aux | grep python | grep {} > delme".format(sys.argv[1]))
+    for line in open('delme').readlines():
         os.system('kill {}'.format(line.strip().split()[1]))
     os.system("rm crap")
 
